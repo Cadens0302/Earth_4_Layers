85e0e7d2f64a8b4c7e256eae813caa1bbf8c4457
Cadens0302 <cadenyxsong@gmail.com>
2026-01-18 17:12:41 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,23 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Layer's of the Earth 🌎")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Coded by Caden Song"
 )
+st.divider()
+
+unit_list = ("Miles(mi)", "Kilometers(km)")
+
+st.header("Find Info")
+st.write("")
+
+st.text_input("Enter Depth")
+
+selected_option = st.selectbox(
+    "Enter Unit for Depth", 
+    unit_list 
+)
+st.write("")
+st.write("")
+
+find = st.button("Find Info")
